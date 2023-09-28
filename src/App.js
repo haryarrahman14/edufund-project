74050e0c7fb70fcb3ecd647001e622809e6f82d5
haryarrahman <haryarrahman14@gmail.com>
2023-09-28 08:13:47 +0700

    upgradade version react scripts

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import './App.css';
-import { useGetBanners } from './hooks/useBanners';
-import { useGetTestimonials } from './hooks/useTestimonials';
+import Navbar from './components/navbar/Navbar';
 
 function App() {
-  useGetBanners();
-  useGetTestimonials();
-
-  return <div className="text-green-500">Test</div>;
+  return <Navbar />;
 }
 
 export default App;
